e2becabaf2278a6519f7c79d8f36af5cc965a4eb
Esteban Jarquin <149350902+Jarking0@users.noreply.github.com>
2025-06-25 16:15:15 -0600

    MaterialController

--- app/Http/Controllers/MaterialController.php
+++ app/Http/Controllers/MaterialController.php
@@ -3,8 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Material;
 
 class MaterialController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $request->validate([
+            'codigo' => 'required|integer|unique:materiales,codigo',
+            'unidadMedida' => 'required|string|max:50',
+            'descripcion' => 'required|string',
+            'ubicacion' => 'required|string|max:100',
+            'idCategoria' => 'required|exists:categorias,idCategoria'
+        ]);
+
+        $material = Material::create([
+            'codigo' => $request->codigo,
+            'unidadMedida' => $request->unidadMedida,
+            'descripcion' => $request->descripcion,
+            'ubicacion' => $request->ubicacion,
+            'idCategoria' => $request->idCategoria
+        ]);
+
+        return response()->json([
+            'mensaje' => 'Material registrado correctamente',
+            'material' => $material
+        ], 201);
+    }
 }
+
